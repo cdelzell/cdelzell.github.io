5b4cf73b1ed335cbdebb6c387acaa48cd7d1b082
cdelzell <claradelzell@gmail.com>
2024-05-30 15:28:46 -0500

    made a whole new html and css file, wanted to refactor a TON

---
+++ homepage.js
@@ -0,0 +1,14 @@
+//animation for intro rectangles
+// document.addEventListener("DOMContentLoaded", () => {
+//     const attribute = document.querySelector(".attribute");
+//     const rect = attribute.innerHTML;
+//     attribute.innerHTML += rect;
+// })
+
+const upperRect1 = document.getElementById('upperRect1');
+const upperRect2 = document.getElementById('upperRect2');
+const upperRect3 = document.getElementById('upperRect3');
+const upperRect4 = document.getElementById('upperRect4');
+const rectArray = {upperRect1, upperRect2, upperRect3, upperRect4};
+
+
