3a98e04b1bbbb7de2f01e6d12b382f5534e14626
cdelzell <claradelzell@gmail.com>
2024-05-31 13:34:39 -0500

    changed image sizing, added footer back

--- homepage.js
+++ homepage.js
@@ -1,42 +1,53 @@
-//animation for intro rectangles
+// //animation for intro rectangles
 
-document.addEventListener("DOMContentLoaded", () => {
-    const secondTrees = document.getElementById('secondTrees');
-    let secondTreesHeight = secondTrees.getBoundingClientRect().height;
-    const blocks = document.getElementById("blocks");
-    const blockUpperRight = document.getElementById('blockUpperRight');
-    const blockUpperLeft = document.getElementById('blockUpperLeft');
-    const blockLowerRight = document.getElementById('blockLowerRight');
-    const blockLowerLeft = document.getElementById('blockLowerLeft');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const secondTrees = document.getElementById('secondTrees');
+//     let secondTreesHeight = secondTrees.getBoundingClientRect().height;
+//     const blocks = document.getElementById("blocks");
+//     const blockUpperRight = document.getElementById('blockUpperRight');
+//     const blockUpperLeft = document.getElementById('blockUpperLeft');
+//     const blockLowerRight = document.getElementById('blockLowerRight');
+//     const blockLowerLeft = document.getElementById('blockLowerLeft');
 
-    const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
-    setHeight(rectArray, secondTreesHeight);
-    setTopMargin(blocks, secondTreesHeight);
-}
-);
+//     const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
+//     setHeight(rectArray, secondTreesHeight);
+//     setTopMargin(blocks, secondTreesHeight);
+// }
+// );
 
-function setHeight(rectArray, height){
-        for(let i = 0; i < rectArray.length; i++){
-            rectArray[i].style.height = `${(height /4)}px`;
+// function setHeight(rectArray, height){
+//         for(let i = 0; i < rectArray.length; i++){
+//             rectArray[i].style.height = `${(height /4)}px`;
+//         }
+//     }
+
+// function setTopMargin(blocks, height){
+//     blocks.style.marginTop = `-${height}px`;
+// }
+
+// document.addEventListener("resize", () => {
+//     const secondTrees = document.getElementById('secondTrees');
+//     let secondTreesHeight = secondTrees.getBoundingClientRect().height;
+//     const blocks = document.getElementById("blocks");
+//     const blockUpperRight = document.getElementById('blockUpperRight');
+//     const blockUpperLeft = document.getElementById('blockUpperLeft');
+//     const blockLowerRight = document.getElementById('blockLowerRight');
+//     const blockLowerLeft = document.getElementById('blockLowerLeft');
+
+//     const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
+//     setHeight(rectArray, secondTreesHeight);
+//     setTopMargin(blocks, secondTreesHeight);
+// }
+// );
+
+
+
+const observer = new IntersectionObserver(entries => {
+    for(let i = 0; i < entries.length; i++){
+        if(entries[i].isIntersection){
+            entries[i].target.classList.add("personalStatementAnimation");
         }
     }
+})
 
-function setTopMargin(blocks, height){
-    blocks.style.marginTop = `-${height}px`;
-}
-
-document.addEventListener("resize", () => {
-    const secondTrees = document.getElementById('secondTrees');
-    let secondTreesHeight = secondTrees.getBoundingClientRect().height;
-    const blocks = document.getElementById("blocks");
-    const blockUpperRight = document.getElementById('blockUpperRight');
-    const blockUpperLeft = document.getElementById('blockUpperLeft');
-    const blockLowerRight = document.getElementById('blockLowerRight');
-    const blockLowerLeft = document.getElementById('blockLowerLeft');
-
-    const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
-    setHeight(rectArray, secondTreesHeight);
-    setTopMargin(blocks, secondTreesHeight);
-}
-);
-
+observer.observe(document.getElementById("hello"));
