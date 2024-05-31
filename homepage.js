ebe8fe5042681c4a398970fcb89db578e210bd97
cdelzell <claradelzell@gmail.com>
2024-05-30 21:09:22 -0500

    have blocks working but NOT resizeable, bad :(

--- homepage.js
+++ homepage.js
@@ -1,14 +1,42 @@
 //animation for intro rectangles
-// document.addEventListener("DOMContentLoaded", () => {
-//     const attribute = document.querySelector(".attribute");
-//     const rect = attribute.innerHTML;
-//     attribute.innerHTML += rect;
-// })
 
-const upperRect1 = document.getElementById('upperRect1');
-const upperRect2 = document.getElementById('upperRect2');
-const upperRect3 = document.getElementById('upperRect3');
-const upperRect4 = document.getElementById('upperRect4');
-const rectArray = {upperRect1, upperRect2, upperRect3, upperRect4};
+document.addEventListener("DOMContentLoaded", () => {
+    const secondTrees = document.getElementById('secondTrees');
+    let secondTreesHeight = secondTrees.getBoundingClientRect().height;
+    const blocks = document.getElementById("blocks");
+    const blockUpperRight = document.getElementById('blockUpperRight');
+    const blockUpperLeft = document.getElementById('blockUpperLeft');
+    const blockLowerRight = document.getElementById('blockLowerRight');
+    const blockLowerLeft = document.getElementById('blockLowerLeft');
 
+    const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
+    setHeight(rectArray, secondTreesHeight);
+    setTopMargin(blocks, secondTreesHeight);
+}
+);
+
+function setHeight(rectArray, height){
+        for(let i = 0; i < rectArray.length; i++){
+            rectArray[i].style.height = `${(height /4)}px`;
+        }
+    }
+
+function setTopMargin(blocks, height){
+    blocks.style.marginTop = `-${height}px`;
+}
+
+document.addEventListener("resize", () => {
+    const secondTrees = document.getElementById('secondTrees');
+    let secondTreesHeight = secondTrees.getBoundingClientRect().height;
+    const blocks = document.getElementById("blocks");
+    const blockUpperRight = document.getElementById('blockUpperRight');
+    const blockUpperLeft = document.getElementById('blockUpperLeft');
+    const blockLowerRight = document.getElementById('blockLowerRight');
+    const blockLowerLeft = document.getElementById('blockLowerLeft');
+
+    const rectArray = [blockUpperRight, blockUpperLeft, blockLowerRight, blockLowerLeft];
+    setHeight(rectArray, secondTreesHeight);
+    setTopMargin(blocks, secondTreesHeight);
+}
+);
 
